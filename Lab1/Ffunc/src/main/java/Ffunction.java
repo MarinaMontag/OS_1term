2d9508f6ec6e577fa7e04a5406fd1396ba02c131
unknown <montag.marinka@gmail.com>
2020-11-08 19:19:38 +0200

    first commit

---
+++ Lab1/Ffunc/src/main/java/Ffunction.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Ffunction {
+    public static String hostName = "localhost";
+    public static int portNumber = 4444;
+
+    public static void main(String[] args) throws InterruptedException {
+        try{
+            Socket pipe=new Socket(hostName,portNumber);
+            BufferedReader in=new BufferedReader(new InputStreamReader(pipe.getInputStream()));
+            PrintWriter out=new PrintWriter(pipe.getOutputStream(),true);
+            Integer x =Integer.parseInt(in.readLine());
+            x=function(x);
+            out.println(x);
+            System.out.println("Hello");
+        } catch (UnknownHostException e) {
+            System.err.println("Unknown host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " + hostName + ".." + e.toString());
+            System.exit(1);
+        }
+    }
+    public static Integer function(Integer x) throws InterruptedException {
+        Thread.sleep(300);
+        x-=6;
+        return x;
+    }
+}
